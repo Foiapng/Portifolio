3cf7261150d6a426c2f1372001d7d9e155c1ef26
Vinicius <vini.familalima@gmail.com>
2025-12-06 15:17:14 -0300

    Ajustes Responsividade

--- front/src/Footer.jsx
+++ front/src/Footer.jsx
@@ -8,10 +8,9 @@ function Footer() {
             <aside><a href="https://wa.me/5511954240586" target="_blank">+55 (11) 95424-0586</a></aside>
             <aside><a href="mailto:viniciuslima.devcontato@gmail.com" target="_blank">Viniciuslima.devcontato@gmail.com</a></aside>
             <aside><a href="https://github.com/Foiapng/Portifolio" target="_blank">Repositorio Github</a></aside>
-            <aside><a href="www.linkedin.com/in/vinicius-lima-3266052bb/" target="_blank">Linkedin</a></aside>
-            <aside>Site feito em React + Vite, imagens e código feitos por mim</aside>
+            <aside><a href="https://www.linkedin.com/in/vinicius-lima-3266052bb" target="_blank">Linkedin</a></aside>
         </section>
-        {/* <p className='Footer__Autoria'>Imagens e código foram feitos por mim</p> */}
+        <aside>Site feito em React + Vite, imagens e código feitos por mim</aside>
     </section>
  );
 }
